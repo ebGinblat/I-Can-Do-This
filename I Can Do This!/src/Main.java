41e33de2624a1880eb2b97b5f8e80cbb23faff4c
ebGinblat <elias.ginblat@stu.ocsb.ca>
2022-11-29 14:38:51 -0500

    Starting screen

--- I Can Do This!/src/Main.java
+++ I Can Do This!/src/Main.java
@@ -1,8 +1,24 @@
-
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please Select a program!");
+		System.out.println("1.Arrays");
+		System.out.println("2.Nested");
+		System.out.println("3.Operators");
+		System.out.println("4.Repetition");
+		System.out.println("5.Selection");
+		System.out.println("6.Sub-Programs");
+		System.out.println("7.Variables");
+		System.out.println("Your choice:");
+		int choice = in.nextInt();
+		if (choice = 1) {
+				System.out.println("You selected Arrays!");
+		}
+	
+
 
 	}
 
